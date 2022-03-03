d35ba984113c3a9705e33a63d4f69588e86b31cb
LeonEngelhardt <engelhardt.leon2000@gmx.de>
2022-03-03 16:15:32 +0100

    3er Commit

--- backend/discord-bot/deps.ts
+++ backend/discord-bot/deps.ts
@@ -1,6 +1,11 @@
 import "https://deno.land/x/dotenv/load.ts";
 
-export { Client, Message, Intents } from "https://deno.land/x/harmony/mod.ts";
+//export{ Client, Message, Intents, } from "https://deno.land/x/harmony/mod.ts";
+export{ Client, Message, GatewayIntents } from "https://deno.land/x/harmony/mod.ts";
+export{ Guild } from "https://deno.land/x/harmony/mod.ts"
+export type { GuildPayload, UserPayload } from "https://deno.land/x/harmony/mod.ts";
+export { Member, User } from "https://deno.land/x/harmony/mod.ts";
+//export type { GuildPayload, MemberData } from "https://deno.land/x/harmony/mod.ts";
 //export { Presence, ClientPresence, Member } from "https://deno.land/x/harmony/mod.ts"; 
 //export { Guild, MembersManager, Member, GuildManager, BaseManager } from "https://deno.land/x/harmony/mod.ts";
-export { Guild } from "https://deno.land/x/harmony/mod.ts"; 
+//export { Guild } from "https://deno.land/x/harmony/mod.ts"; 
