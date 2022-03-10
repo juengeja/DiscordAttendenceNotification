9153865bceb0b073a2f17683362ad58bdc4fdd57
Jannis Jüngert <73344489+juengeja@users.noreply.github.com>
2022-03-10 18:08:53 +0100

    Discord attendance update

--- backend/discord-bot/discordbot.ts
+++ backend/discord-bot/discordbot.ts
@@ -1,4 +1,4 @@
-import{ Client, Message, GatewayIntents, MembersManager, UsersManager } from "https://code.harmony.rocks/c2565e60ffc52580368f99bfc970832a19ad37b2";
+import{ Client, Message, GatewayIntents, MembersManager, UsersManager, Presence } from "https://code.harmony.rocks/c2565e60ffc52580368f99bfc970832a19ad37b2";
 import "https://deno.land/x/dot_env@0.2.0/load.ts"
 
 
@@ -30,42 +30,32 @@ if(guild == undefined){
         if (content == "!Presence") {
             // deno-lint-ignore prefer-const
             let authorPresence = await guild.presences.resolve(msg.author.id);
-            msg.reply(`Your presence status: ${authorPresence?.status}`);
+            msg.reply(`Your presence status: \`${authorPresence?.status}\``);
         }
     });
-
-
+    
     const m = new MembersManager(client, guild);
     const membersList = await m.fetchList(10)
     membersList;
-
+    
     const usersman = new UsersManager(client)
-
+    
     const memberLeon = await guild.members.resolve('672215070352211968')
     if(memberLeon != undefined){
         console.log(memberLeon.id)
     }
-
+    
     const userLeon = await usersman.fetch('672215070352211968')
     console.log(userLeon.id)
-
+    
     const _userIDSinneckLAP = '946431511983448064'
     const myPresence = await guild.presences.resolve('672215070352211968')
     console.log(myPresence?.status)
 
-}
-/*
-client.on("presenceUpdate", (memberLeon, newGuildMember) => {
-    if (memberLeon.id !== "YOURID") return false; // Checking if the GuildMember is a specific user.
-
-    if (memberLeon.presence.status !== newGuildMember.presence.status) { // Checking if the Presence is the same.
-        if (newGuildMember.presence.status == "online") { // Checking if the GuildMember is online.
-            const Channel = client.channels.get("CHANNELID");
-            if (!Channel) return console.error("Invalid channel.");
-            if (newGuildMember.guild.id !== Channel.guild.id) return false; // Making sure the Message gets sent once.
-
-            Channel.send(`${newGuildMember.user.tag} is now online!`);
+    client.on("presenceUpdate", (presenceUpdate: Presence) => {
+        if(presenceUpdate.status == 'online'){
+            client.channels.sendMessage('947821198560100372', `Der User \`${presenceUpdate.user.username}\` hat seinen Status auf \`${presenceUpdate.status}\` geändert!`, );
         }
-    }
-})
-*/
+    })
+
+}
