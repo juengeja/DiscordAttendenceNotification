eec93ab1c9bc7f10e0feeea11d11f00b4f945f91
LeonEngelhardt <engelhardt.leon2000@gmx.de>
2022-03-17 17:06:51 +0100

    Anpassungen Website

---
+++ frontend/discordAttendence/src/components/Login.svelte
@@ -0,0 +1,53 @@
+
+
+<script>
+//import Home from "./Home.svelte";
+
+  let Chat_ID = "";
+  let listOfChat_IDs = [];
+
+  function handleClick(){
+    listOfChat_IDs.push(Chat_ID);
+    alert(JSON.stringify(listOfChat_IDs));
+  }   
+  
+  
+  function klicken(){
+    console.dir(Home);
+    
+    /*window.location=*/
+
+    // (in Script)
+    /*<a href="../components/Home.svelte" target="_self">Home</a>
+    <a href="Login.svelte" target="_self">Login</a>*/
+  }
+
+</script>
+
+  <h1><b>Please enter your Chat-ID:</b></h1>
+  <p>
+    Contact our <a href="https://t.me/discordattendancenotificationbot">Telegram-Bot</a> to get your Chat-ID.
+  </p>
+
+  <div id="Login" class="tabcontent">
+    <input bind:value={Chat_ID} />
+  </div>
+
+      {#if Chat_ID.length > 5}
+    <button type="submit" on:click={handleClick}>Submit</button>
+  {/if}
+  
+  <!--
+  <link
+  href="https://fonts.googleapis.com/css?family=Overpass:100,400"
+  rel="stylesheet"
+  />
+  -->
+  
+<style>
+  h1 {
+    color: #ff7b00;
+    font-size: 5em;
+    font-weight: 200;
+  }
+</style>
