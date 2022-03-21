abedfab03e5341696e39a26d9292a5b1547f7bda
Jannis Jüngert <73344489+juengeja@users.noreply.github.com>
2022-03-21 01:02:34 +0100

    aloedb and telegrambot update

--- backend/denodb.ts
+++ backend/denodb.ts
@@ -1,21 +1,72 @@
 import { Database } from 'https://deno.land/x/aloedb/mod.ts';
+export { insertChat, insertChannel, insertGuild, insertUser };
+export { findChatByChatID, findChannelByChatID, findGuildByChatID, findUserByChatID };
+export { findManyChannelByChatID, findManyGuildByChatID, findManyUserByChatID };
 
-interface Channels {
+interface Chat {
+    chatID: number
+}
+
+interface Channel {
     channelID: number,
     chatID: number
 }
 
-const db = new Database<Channels>({path:"./channels.json", autosave:true});
+interface Guild {
+    guildID: number,
+    chatID: number
+}
 
-//await db.insertOne({channelID:123456,chatID:654321})
-//await db.insertOne({channelID:23,chatID:2})
+interface User {
+    userID: number,
+    chatID: number
+}
 
-await db.save()
+const chatDB = new Database<Chat>({path:"./Chat.json", autosave:true});
+const channelDB = new Database<Channel>({path:"./Channel.json", autosave:true});
+const guildDB = new Database<Guild>({path:"./Guild.json", autosave:true});
+const userDB = new Database<User>({path:"./User.json", autosave:true});
 
-let reply = await db.findOne({channelID:2})
-console.log(reply)
-reply = await db.findOne({chatID:2})
-console.log(reply?.channelID)
+function insertChat(chat: Chat){
+    chatDB.insertOne(chat)
+}
 
-const arrayReply = await db.findMany({chatID:2})
-console.log(arrayReply)
+function insertChannel(channel: Channel){
+    channelDB.insertOne(channel)
+}
+
+function insertGuild(guild: Guild){
+    guildDB.insertOne(guild)
+}
+
+function insertUser(user: User){
+    userDB.insertOne(user)
+}
+
+function findChatByChatID(chatID: number){
+    return chatDB.findOne({chatID:chatID})
+}
+
+function findChannelByChatID(chatID: number){
+    return channelDB.findOne({chatID:chatID})
+}
+
+function findGuildByChatID(chatID: number){
+    return guildDB.findOne({chatID:chatID})
+}
+
+function findUserByChatID(chatID: number){
+    return userDB.findOne({chatID:chatID})
+}
+
+function findManyChannelByChatID(chatID: number){
+    return channelDB.findMany({chatID:chatID})
+}
+
+function findManyGuildByChatID(chatID: number){
+    return guildDB.findMany({chatID:chatID})
+}
+
+function findManyUserByChatID(chatID: number){
+    return userDB.findMany({chatID:chatID})
+}
