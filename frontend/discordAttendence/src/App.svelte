a38fd7793f2fa1729fd088494f96ec3bcdfece16
LeonEngelhardt <engelhardt.leon2000@gmx.de>
2022-04-10 20:33:17 +0200

    design changes Home.svelte v1

--- frontend/discordAttendence/src/App.svelte
+++ frontend/discordAttendence/src/App.svelte
@@ -6,6 +6,29 @@
   let page;
   let params;
 
+
+
+  async function handleClick(){
+
+    alert(Chat_ID);
+
+    const exists = await fetch(`http://localhost:8800/api/v1/persistence/get/chatId/${Chat_ID}`, {signal: signal, method: "GET"})
+
+    if(exists){
+    window.location.href = "./Home.svelte"
+    }
+  }
+
+
+  let Chat_ID;
+
+  const addChatID = (event) => {
+    Chat_ID = [Chat_ID, event.detail];
+  };
+
+  //let Chat_ID = 123456789;
+  //alert(chat_ID);
+
   router('/', () => page = Login)
   router('/Home.svelte', (Context, next) => {
     params = Context.params;
@@ -16,12 +39,19 @@
   
 </script>
 
+<!--
+<Login bind:Chat_ID/>
+  <p>{Chat_ID}</p>
+-->
+
 {#if page === Login}
-<Login />
+<!--<Login bind:value={Chat_ID}/>-->
+  <Login {Chat_ID} handleClick={handleClick}/>
 {/if}
 
 {#if page === Home}
-<Home />
+<!---->
+<Home {Chat_ID}/>
 {/if}
 
 <style>
